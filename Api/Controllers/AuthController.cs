8df118410451fef14f6737ba53ddb313a24562c8
abassadebayo <greatmoh007@gmail.com>
2026-02-08 17:27:19 +0100

    Create Organization Controller

---
+++ Api/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using Application.DTOs;
+using Application.DTOs.Auth;
+using Application.Interfaces.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController(IOrganizationService organizationService) : ControllerBase
+    {
+        private readonly IOrganizationService _organizationService = organizationService ?? throw new ArgumentNullException(nameof(organizationService));
+
+        [HttpPost("register")]
+        [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(BaseResponse))]
+        public async Task<IActionResult> RegisterNewAccount([FromBody] RegisterOrganizationRequestModel request)
+        {
+            var registerOrg = await _organizationService.RegisterOrganizationAsync(request);
+            return Ok(registerOrg);
+        }
+
+        
+    }
+}
---
+++ Api/Controllers/OrganizationsController.cs
@@ -0,0 +1,32 @@
+﻿using Application.DTOs;
+using Application.DTOs.Auth;
+using Application.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Security.Claims;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class OrganizationsController(IOrganizationService organizationService) : ControllerBase
+    {
+        private readonly IOrganizationService _organizationService = organizationService ?? throw new ArgumentNullException(nameof(organizationService));
+
+        [HttpPost]
+        [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(BaseResponse))]
+        public async Task<IActionResult> CreateOrganization([FromBody] AddOrganizationToExistingAdminRequest request)
+        {
+            var userIdString = User?.FindFirst(ClaimTypes.NameIdentifier)!.Value;
+            if (!Guid.TryParse(userIdString, out Guid adminUserId))
+            {
+                return BadRequest("Invalid userId");
+            }
+            var createOrg = await _organizationService.AddExistingAdminToOrganizationAsync(adminUserId, request);
+            return Ok(createOrg);
+        }
+    }
+}
