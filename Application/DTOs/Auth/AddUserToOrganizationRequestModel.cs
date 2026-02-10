d6ba7cebd7781b7baed8410b814746645749da3b
abassadebayo <greatmoh007@gmail.com>
2026-02-10 11:58:39 +0100

    Invite BuildHedge Users to Organization

---
+++ Application/DTOs/Auth/AddUserToOrganizationRequestModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.DTOs.Auth
+{
+    public record AddUserToOrganizationRequestModel(
+        string Email,
+        string FirstName,
+        string LastName,
+        string PhoneNumber,
+        string ProfilePictureUrl,
+        Guid OrganizationId,
+        Guid RoleId
+
+        );
+    
+}
---
+++ Application/Interfaces/Services/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using Application.DTOs.Auth;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Interfaces.Services
+{
+    public interface IUserService
+    {
+        public Task<AuthResponse> InviteUserToOrganizationAsync(Guid adminUserId, AddUserToOrganizationRequestModel request);
+    }
+}
