b17072a314937bed92cb8eac8d85c25d163af6c1
abassadebayo <greatmoh007@gmail.com>
2026-02-24 16:40:53 +0100

    Update Hedge contract with flags for background worker notice

---
+++ Infrastructure/Migrations/20260224153837_AddNoticeFlagsToHedgeEntity.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNoticeFlagsToHedgeEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "IsFinalNoticeSent",
+                table: "HedgeContracts",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "IsSevenDayNoticeSent",
+                table: "HedgeContracts",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Memberships",
+                keyColumns: new[] { "OrganizationId", "UserId" },
+                keyValues: new object[] { new Guid("c8f2e6ab-9f34-4b97-8b7c-1a5e86d78e42"), new Guid("c8f2e5ab-9f34-4b97-8b7c-1a5e86c77e42") },
+                column: "CreatedAtUtc",
+                value: new DateTime(2026, 2, 24, 15, 38, 34, 849, DateTimeKind.Utc).AddTicks(7486));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsFinalNoticeSent",
+                table: "HedgeContracts");
+
+            migrationBuilder.DropColumn(
+                name: "IsSevenDayNoticeSent",
+                table: "HedgeContracts");
+
+            migrationBuilder.UpdateData(
+                table: "Memberships",
+                keyColumns: new[] { "OrganizationId", "UserId" },
+                keyValues: new object[] { new Guid("c8f2e6ab-9f34-4b97-8b7c-1a5e86d78e42"), new Guid("c8f2e5ab-9f34-4b97-8b7c-1a5e86c77e42") },
+                column: "CreatedAtUtc",
+                value: new DateTime(2026, 2, 24, 11, 18, 9, 92, DateTimeKind.Utc).AddTicks(7300));
+        }
+    }
+}
